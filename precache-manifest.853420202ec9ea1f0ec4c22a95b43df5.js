d1603745de8854e9c832d54a946341c081b1d80f
Agrotecnic <deyvidrb@icloud.com>
2024-11-26 07:14:43 +0000

    Updates

---
+++ precache-manifest.853420202ec9ea1f0ec4c22a95b43df5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9618ee9d1d84c73b5d0cddda076a5b",
+    "url": "/statusGD/index.html"
+  },
+  {
+    "revision": "9af9fcf880d873e07526",
+    "url": "/statusGD/static/css/main.0e182933.chunk.css"
+  },
+  {
+    "revision": "988d4eea2524e63420e0",
+    "url": "/statusGD/static/js/2.deb8be70.chunk.js"
+  },
+  {
+    "revision": "ee3b9cc7af88da4df6fa",
+    "url": "/statusGD/static/js/3.74e8da61.chunk.js"
+  },
+  {
+    "revision": "d1cdeab6e8897ef91f11",
+    "url": "/statusGD/static/js/4.a2a3e056.chunk.js"
+  },
+  {
+    "revision": "9af9fcf880d873e07526",
+    "url": "/statusGD/static/js/main.00270a66.chunk.js"
+  },
+  {
+    "revision": "10bb2d9f249d3929429a",
+    "url": "/statusGD/static/js/runtime~main.037ea5ee.js"
+  }
+]);
